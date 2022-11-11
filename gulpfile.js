15047917b36bf8265c4d3d13383c0499d0b5f86f
Luca De Menego <lucademenego99@gmail.com>
2022-11-11 12:02:54 +0100

    Add white.css into the redbean file

--- gulpfile.js
+++ gulpfile.js
@@ -21,10 +21,10 @@ export default (cb) => {
 
         console.log("Build completed! Setting up offline mode");
 
-        child_process.exec("cd dist/base/ && chmod +x ../../bin/zip.com && ../../bin/zip.com -r redbean.com utils full-offline.iife.js reveal.js reveal.css blood.css custom-style.css && ../../bin/zip.com -0 redbean.com index.html && chmod +x redbean.com", (err, stdout, stderr) => {
+        child_process.exec("cd dist/base/ && chmod +x ../../bin/zip.com && ../../bin/zip.com -r redbean.com utils full-offline.iife.js reveal.js reveal.css blood.css white.css custom-style.css && ../../bin/zip.com -0 redbean.com index.html && chmod +x redbean.com", (err, stdout, stderr) => {
             if (err) {
                 // Try to construct the path for windows systems
-                child_process.exec("cd dist\\base\\ && ..\\..\\bin\\zip.com -r redbean.com full-offline.iife.js reveal.js reveal.css blood.css custom-style.css && ..\\..\\bin\\zip.com -r -0 redbean.com utils index.html", (err, stdout, stderr) => {
+                child_process.exec("cd dist\\base\\ && ..\\..\\bin\\zip.com -r redbean.com full-offline.iife.js reveal.js reveal.css blood.css white.css custom-style.css && ..\\..\\bin\\zip.com -r -0 redbean.com utils index.html", (err, stdout, stderr) => {
                     if (err) {
                         console.log("[ERROR] zipping dist into redbean.com");
                         cb(err);

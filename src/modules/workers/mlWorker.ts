76a04a02bb8a33b52e95292bd18febfee16cd6f1
Luca De Menego <lucademenego99@gmail.com>
2023-01-31 11:12:41 +0100

    Check for evaluation errors with standard-ml

--- src/modules/workers/mlWorker.ts
+++ src/modules/workers/mlWorker.ts
@@ -4,10 +4,23 @@ onmessage = async (e) => {
     try {
         const initialState = getFirstState();
         const interpretationResult = interpret(e.data.script, initialState);
+        // CHeck for evaluation errors
+        if (interpretationResult.evaluationErrored) {
+            throw new Error(interpretationResult.error?.toString(interpretationResult.state));
+        }
+
+        // Check for warnings
+        let warnings = "";
+        for(let i = 0; i < interpretationResult.warnings.length; i++) {
+            warnings += '[WARNING]: ' + interpretationResult.warnings[i] + "\n";
+        }
+
+        // Return the result
         postMessage({
-            "debug": interpretationResult.state.toString({stopId: initialState.id + 1})
+            "debug": warnings + (warnings != "" ? "\n" : "") + interpretationResult.state.toString({stopId: initialState.id + 1})
         });
     } catch (e) {
+        console.log(e);
         postMessage({
             "error": e
         });

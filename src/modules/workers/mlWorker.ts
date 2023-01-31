08b4872382c2cfea5ae50008c8e7d51fe3c50f8d
Luca De Menego <lucademenego99@gmail.com>
2023-01-31 12:16:28 +0100

    Return all standardml interpretation results

--- src/modules/workers/mlWorker.ts
+++ src/modules/workers/mlWorker.ts
@@ -2,22 +2,34 @@ import { interpret, getFirstState } from '@sosml/interpreter';
 
 onmessage = async (e) => {
     try {
+        let interpretationResult;
+        let result = "";
         const initialState = getFirstState();
-        const interpretationResult = interpret(e.data.script, initialState);
-        // CHeck for evaluation errors
-        if (interpretationResult.evaluationErrored) {
-            throw new Error(interpretationResult.error?.toString(interpretationResult.state));
-        }
 
-        // Check for warnings
-        let warnings = "";
-        for(let i = 0; i < interpretationResult.warnings.length; i++) {
-            warnings += '[WARNING]: ' + interpretationResult.warnings[i] + "\n";
+        // Split the script: each instruction is delimited by a ;
+        const script = e.data.script.split(';');
+
+        // Interpret the script
+        for(let i = 0; i < script.length-1; i++) {
+            script[i] += ";";
+            interpretationResult = interpret(script[i], i == 0 ? initialState : interpretationResult.state);
+            // CHeck for evaluation errors
+            if (interpretationResult.evaluationErrored) {
+                throw new Error(interpretationResult.error?.toString(interpretationResult.state));
+            }
+
+            // Check for warnings
+            let warnings = "";
+            for(let i = 0; i < interpretationResult.warnings.length; i++) {
+                warnings += '[WARNING]: ' + interpretationResult.warnings[i] + "\n";
+            }
+
+            result += warnings + (warnings != "" ? "\n" : "") + interpretationResult.state.toString({stopId: interpretationResult.state.id});
         }
 
         // Return the result
         postMessage({
-            "debug": warnings + (warnings != "" ? "\n" : "") + interpretationResult.state.toString({stopId: initialState.id + 1})
+            "debug": result
         });
     } catch (e) {
         console.log(e);
